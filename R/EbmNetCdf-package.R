b7b81f603d9dec4dd206bae82e48b49e28d32f64
Christof Schoetz <cschoetz@pik-potsdam.de>
2023-11-16 12:46:28 +0100

    fix

--- R/EbmNetCdf-package.R
+++ R/EbmNetCdf-package.R
@@ -4,7 +4,7 @@
 #' @importFrom dplyr lst filter
 #' @importFrom tibble tibble
 #' @importFrom rlang .env .data
-#' @importFrom RNetCDF open.nc var.get.nc close.nc create.nc var.def.nc dim.def.nc file.inq.nc var.put.nc
+#' @importFrom RNetCDF open.nc var.get.nc close.nc create.nc var.def.nc dim.def.nc file.inq.nc var.put.nc read.nc
 NULL
 
 ## usethis namespace: start
