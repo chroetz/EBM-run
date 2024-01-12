18e6db36ee1d3a088521fcd451a616fe92530f12
Christof Schoetz <cschoetz@pik-potsdam.de>
2024-01-12 16:28:34 +0100

    add more log

--- R/maskSum.R
+++ R/maskSum.R
@@ -36,12 +36,15 @@ runMaskSummation <- function() {
 
   maskScalingValues <- NULL
   for (regionName in regionNames) {
+    pt <- proc.time()
+    cat("Process region '", regionName, "' ... ", sep="")
     maskValues <- getMaskValues(regionName, maskList)
     if (is.null(maskScalingValues)) {
       maskScalingValues <- maskValues
     } else {
       maskScalingValues <- maskScalingValues + maskValues
     }
+    cat("Done in ", (proc.time()-pt)[3], "s.\n")
   }
 
   cat("Close mask NC-File ... ")
